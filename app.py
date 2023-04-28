9c5307917b2b60d8635f60ad8fe7920d5dd46a2d
nigussolomon <nigussolomon444@gmail.com>
2023-04-29 00:41:07 +0300

    some configs added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from books import router as booksRouter
 from favourites import router as favouritesRouter
 from downloads import router as downloadsRouter
@@ -12,3 +13,6 @@ async def root():
 app.include_router(booksRouter, tags=["books"]) # prefix="/books" is used to add a prefix to all the routes in the booksRouter (booksRouter
 app.include_router(favouritesRouter, tags=["favourites"]) # prefix="/favourites" is used to add a prefix to all the routes in the favouritesRouter (favouritesRouter
 app.include_router(downloadsRouter, tags=["downloads"])
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
